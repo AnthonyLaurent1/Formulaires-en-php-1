661c6e624032d608c83dfe4b3fa3cc88d0ecd258
anthony laurent <anthony061214@gmail.com>
2021-10-04 12:14:30 +0200

    modif

--- form.php
+++ form.php
@@ -1,23 +1,23 @@
 <form action="thanks.php" method="post">
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_lastname">
+        <input type="text" id="nom" name="user_lastname" required>
     </div>
     <div>
         <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="user_firstname">
+        <input type="text" id="prenom" name="user_firstname" required>
     </div>
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
+        <input type="email" id="courriel" name="user_email" required>
     </div>
     <div>
         <label for="telephone">Téléphone :</label>
-        <input type="tel" id="telephone" name="user_tel">
+        <input type="tel" id="telephone" name="user_tel" required>
     </div>
     <div>
         <label for="sujet">Choix :</label>
-        <select id="sujet" name="user_sujet">
+        <select id="sujet" name="user_sujet" required>
             <option>Votre choix :</option>
             <option>Vacances au soleil</option>
             <option>Vacances à la neige</option>
@@ -26,7 +26,7 @@
     </div>
     <div>
         <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
+        <textarea id="message" name="user_message" required></textarea>
     </div>
     <div>
         <button type="submit">Envoyer votre message</button>
